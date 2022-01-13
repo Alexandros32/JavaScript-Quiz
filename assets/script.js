cf5b50a80b360e0857469e7297573615ccf3f630
Alexandros32 <81386885+Alexandros32@users.noreply.github.com>
2022-01-13 22:26:16 +0000

    add script and comments

--- assets/script.js
+++ assets/script.js
@@ -39,6 +39,7 @@ let counter;
 let counterLine;
 let widthValue = 0;
 
+// The code was taken by https://www.codingnepalweb.com/ 
 const restart_quiz = result_box.querySelector(".buttons .restart");
 const quit_quiz = result_box.querySelector(".buttons .quit");
 
@@ -60,3 +61,54 @@ restart_quiz.onclick = ()=>{
     timeText.textContent = "Time Left";
     next_btn.classList.remove("show");
 }
+
+// The code was taken by https://www.codingnepalweb.com/ 
+quit_quiz.onclick = ()=>{
+    window.location.reload(); 
+}
+
+// The code was taken by https://www.codingnepalweb.com/ 
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+// The code was taken by https://www.codingnepalweb.com/ 
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){ //if question count is less than total question length
+        que_count++; //increment the que_count value
+        que_numb++; //increment the que_numb value
+        showQuetions(que_count); //calling showQestions function
+        queCounter(que_numb); //passing que_numb value to queCounter
+        clearInterval(counter); //clear counter
+        clearInterval(counterLine); //clear counterLine
+        startTimer(timeValue); //calling startTimer function
+        startTimerLine(widthValue); //calling startTimerLine function
+        timeText.textContent = "Time Left"; //change the timeText to Time Left
+        next_btn.classList.remove("show"); //hide the next button
+    }else{
+        clearInterval(counter); //clear counter
+        clearInterval(counterLine); //clear counterLine
+        showResult(); //calling showResult function
+    }
+}
+
+// The code was taken by https://www.codingnepalweb.com/ 
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
+
+    //creating a new span and div tag for question and option and passing the value using array index
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+    
+    const option = option_list.querySelectorAll(".option");
+
+    // The code was taken by https://www.codingnepalweb.com/ 
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
