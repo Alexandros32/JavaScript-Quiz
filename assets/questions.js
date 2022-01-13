91a80a239a88c82eb0f2d78f7ea1c41f46a65dd5
Alexandros32 <81386885+Alexandros32@users.noreply.github.com>
2022-01-13 22:41:13 +0000

    Add questions and comments

---
+++ assets/questions.js
@@ -0,0 +1,131 @@
+// The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+let questions = [
+    {
+    numb: 1,
+    question: "What is the capital of Greece?",
+    answer: "Athens",
+    options: [
+      "Thessaloniki",
+      "Larissa",
+      "Athens",
+      "Patra"
+    ]
+  },
+
+  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+    {
+    numb: 2,
+    question: "What is the capital of England?",
+    answer: "London",
+    options: [
+      "Manchester",
+      "London",
+      "Birmingham",
+      "Liverpool"
+    ]
+  },
+
+  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+    {
+    numb: 3,
+    question: "What is the capital of Italy?",
+    answer: "Rome",
+    options: [
+      "Milan",
+      "Naples",
+      "Turin",
+      "Rome"
+    ]
+  },
+
+  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+    {
+    numb: 4,
+    question: "What is the capital of France?",
+    answer: "Paris",
+    options: [
+      "Paris",
+      "Marseille",
+      "Lyon",
+      "Toulouse"
+    ]
+  },
+
+  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+    {
+    numb: 5,
+    question: "What is the capital of Spain?",
+    answer: "Madrid",
+    options: [
+      "Barcelona",
+      "Valencia",
+      "Madrid",
+      "Sevilla"
+    ]
+  },
+
+  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+  {
+    numb: 6,
+    question: "What is the capital of Germany?",
+    answer: "Berlin",
+    options: [
+      "Munich",
+      "Berlin",
+      "Hamburg",
+      "Frankfurt"
+    ]
+  },
+
+  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+  {
+  numb: 7,
+  question: "What is the capital of Portugal?",
+  answer: "Lisbon",
+  options: [
+    "Porto",
+    "Braga",
+    "Coimbra",
+    "Lisbon"
+  ]
+},
+
+// The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+{
+  numb: 8,
+  question: "What is the capital of Scotland?",
+  answer: "Edinburgh",
+  options: [
+    "Edinburgh",
+    "Glasgow",
+    "Aberdeen",
+    "Dundee"
+  ]
+},
+
+// The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+{
+  numb: 9,
+  question: "What is the capital of Switzerland?",
+  answer: "Bern",
+  options: [
+    "Zurich",
+    "Geneva",
+    "Basel",
+    "Bern"
+  ]
+},
+
+// The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+{
+  numb: 10,
+  question: "What is the capital of Austria?",
+  answer: "Vienna",
+  options: [
+    "Salzburg",
+    "Vienna",
+    "Graz",
+    "Linz"
+  ]
+},
+];
