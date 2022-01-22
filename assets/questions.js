7ffe769596f31b57d5bf61f8cc29b5b81a5537a6
Alexandros32 <81386885+Alexandros32@users.noreply.github.com>
2022-01-22 22:08:38 +0000

    clean the questions js

--- assets/questions.js
+++ assets/questions.js
@@ -1,93 +1,57 @@
 // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
-let questions = [
-    {
-    numb: 1,
-    question: "What is the capital of Greece?",
-    answer: "Athens",
-    options: [
-      "Thessaloniki",
-      "Larissa",
-      "Athens",
-      "Patra"
-    ]
-  },
+let questions = [{
+  numb: 1,
+  question: "What is the capital of Greece?",
+  answer: "Athens",
+  options: ["Thessaloniki", "Larissa", "Athens", "Patra"]
+},
 
-  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
-    {
-    numb: 2,
-    question: "What is the capital of England?",
-    answer: "London",
-    options: [
-      "Manchester",
-      "London",
-      "Birmingham",
-      "Liverpool"
-    ]
-  },
+// The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+{
+  numb: 2,
+  question: "What is the capital of England?",
+  answer: "London",
+  options: ["Manchester", "London", "Birmingham", "Liverpool"]
+},
 
-  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
-    {
-    numb: 3,
-    question: "What is the capital of Italy?",
-    answer: "Rome",
-    options: [
-      "Milan",
-      "Naples",
-      "Turin",
-      "Rome"
-    ]
-  },
+// The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+{
+  numb: 3,
+  question: "What is the capital of Italy?",
+  answer: "Rome",
+  options: ["Milan", "Naples", "Turin", "Rome"]
+},
 
-  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
-    {
-    numb: 4,
-    question: "What is the capital of France?",
-    answer: "Paris",
-    options: [
-      "Paris",
-      "Marseille",
-      "Lyon",
-      "Toulouse"
-    ]
-  },
+// The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+{
+  numb: 4,
+  question: "What is the capital of France?",
+  answer: "Paris",
+  options: ["Paris", "Marseille", "Lyon", "Toulouse"]
+},
 
-  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
-    {
-    numb: 5,
-    question: "What is the capital of Spain?",
-    answer: "Madrid",
-    options: [
-      "Barcelona",
-      "Valencia",
-      "Madrid",
-      "Sevilla"
-    ]
-  },
+// The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+{
+  numb: 5,
+  question: "What is the capital of Spain?",
+  answer: "Madrid",
+  options: ["Barcelona", "Valencia", "Madrid", "Sevilla"]
+},
 
-  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
-  {
-    numb: 6,
-    question: "What is the capital of Germany?",
-    answer: "Berlin",
-    options: [
-      "Munich",
-      "Berlin",
-      "Hamburg",
-      "Frankfurt"
-    ]
-  },
+// The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+{
+  numb: 6,
+  question: "What is the capital of Germany?",
+  answer: "Berlin",
+  options: ["Munich", "Berlin", "Hamburg", "Frankfurt"]
+},
 
-  // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
-  {
+// The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
+{
   numb: 7,
   question: "What is the capital of Portugal?",
   answer: "Lisbon",
-  options: [
-    "Porto",
-    "Braga",
-    "Coimbra",
-    "Lisbon"
-  ]
+  options: ["Porto", "Braga", "Coimbra", "Lisbon"]
 },
 
 // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
@@ -95,12 +59,7 @@ let questions = [
   numb: 8,
   question: "What is the capital of Scotland?",
   answer: "Edinburgh",
-  options: [
-    "Edinburgh",
-    "Glasgow",
-    "Aberdeen",
-    "Dundee"
-  ]
+  options: ["Edinburgh", "Glasgow", "Aberdeen", "Dundee"]
 },
 
 // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
@@ -108,12 +67,7 @@ let questions = [
   numb: 9,
   question: "What is the capital of Switzerland?",
   answer: "Bern",
-  options: [
-    "Zurich",
-    "Geneva",
-    "Basel",
-    "Bern"
-  ]
+  options: ["Zurich", "Geneva", "Basel", "Bern"]
 },
 
 // The code was taken by https://www.codingnepalweb.com/ and i have adjust it in my needs
@@ -121,11 +75,6 @@ let questions = [
   numb: 10,
   question: "What is the capital of Austria?",
   answer: "Vienna",
-  options: [
-    "Salzburg",
-    "Vienna",
-    "Graz",
-    "Linz"
-  ]
+  options: ["Salzburg", "Vienna", "Graz", "Linz"]
 },
 ];
